030468db8131f03cc68375b749e4367a9869142a
Wai Linn Kyaw <stephensuu03@gmail.com>
2023-11-05 11:52:35 +0630

    3D Play Integrate

---
+++ resources/views/frontend/threed.blade.php
@@ -0,0 +1,178 @@
+@extends('user_layouts.master')
+
+@section('content')
+<!-- remain balance section -->
+<div class="fixed-card">
+    <div class="row mt-2">
+      <div class="col d-flex justify-content-between">
+       <div>
+         <div class="d-flex">
+           <span class="material-icons" style="font-size: 40px;">account_balance_wallet</span>
+           <p class="mt-2 px-2">လက်ကျန်ငွေ</p>
+         </div>
+         <p>0.00 Kyat</p>
+       </div>
+       <div class="text-center">
+        <i class="fa-solid fa-clock mt-1" style="font-size: 28px;"></i><span>ပိတ်ရန်ကျန်ချိန်</span>
+        <p>2023-11-16 <br><span>02:30:00PM</span></p>
+       </div>
+      </div>
+    </div>
+    <!-- event-card -->
+    <div class="card event-card">
+        <div class="d-flex p-1 justify-content-around">
+            <div>
+                <a href="">
+                <span class="material-icons">manage_search</span>
+                <p>မှတ်တမ်း</p>
+                </a>
+            </div>
+            <div>
+                <a href="3d_winner_page.html">
+                <span class="material-icons">stars</span>
+                <p>ကံထူးရှင်များ</p>
+                </a>
+            </div>
+            <div>
+                <a href="threed_history.html">
+                <span class="material-icons">event_note</span>
+                <p>ထွက်ဂဏန်းများ</p>
+                </a>
+            </div>
+        </div>
+    </div>
+    <!-- event card -->
+</div>
+<!-- remain balance section -->
+
+<!-- winner result -->
+<div class="container-fluid mb-5 pb-3" style="margin-top:30px;min-height: 100vh;">
+    <div class="bg-green p-3 rounded my-3">
+        <div class="d-flex justify-content-between">
+          <div class="text-center">
+            <p class="mb-0 pb-0">Date</p>
+            <p class="mb-0 pb-0">16-10-2023</p>
+          </div>
+          <div>
+            <p class="mb-0 pb-0">3D</p>
+            <p class="text-warning mb-0 pb-0">445</p>
+          </div>
+        </div>
+    </div>
+    <div class="bg-green p-3 rounded my-3">
+        <div class="d-flex justify-content-between">
+          <div class="text-center">
+            <p class="mb-0 pb-0">Date</p>
+            <p class="mb-0 pb-0">16-10-2023</p>
+          </div>
+          <div>
+            <p class="mb-0 pb-0">3D</p>
+            <p class="text-warning mb-0 pb-0">445</p>
+          </div>
+        </div>
+    </div>
+    <div class="bg-green p-3 rounded my-3">
+        <div class="d-flex justify-content-between">
+          <div class="text-center">
+            <p class="mb-0 pb-0">Date</p>
+            <p class="mb-0 pb-0">16-10-2023</p>
+          </div>
+          <div>
+            <p class="mb-0 pb-0">3D</p>
+            <p class="text-warning mb-0 pb-0">445</p>
+          </div>
+        </div>
+    </div>
+    <div class="bg-green p-3 rounded my-3">
+        <div class="d-flex justify-content-between">
+          <div class="text-center">
+            <p class="mb-0 pb-0">Date</p>
+            <p class="mb-0 pb-0">16-10-2023</p>
+          </div>
+          <div>
+            <p class="mb-0 pb-0">3D</p>
+            <p class="text-warning mb-0 pb-0">445</p>
+          </div>
+        </div>
+    </div>
+    <div class="bg-green p-3 rounded my-3">
+        <div class="d-flex justify-content-between">
+          <div class="text-center">
+            <p class="mb-0 pb-0">Date</p>
+            <p class="mb-0 pb-0">16-10-2023</p>
+          </div>
+          <div>
+            <p class="mb-0 pb-0">3D</p>
+            <p class="text-warning mb-0 pb-0">445</p>
+          </div>
+        </div>
+    </div>
+    <div class="bg-green p-3 rounded my-3">
+        <div class="d-flex justify-content-between">
+          <div class="text-center">
+            <p class="mb-0 pb-0">Date</p>
+            <p class="mb-0 pb-0">16-10-2023</p>
+          </div>
+          <div>
+            <p class="mb-0 pb-0">3D</p>
+            <p class="text-warning mb-0 pb-0">445</p>
+          </div>
+        </div>
+    </div>
+    <div class="bg-green p-3 rounded my-3">
+        <div class="d-flex justify-content-between">
+          <div class="text-center">
+            <p class="mb-0 pb-0">Date</p>
+            <p class="mb-0 pb-0">16-10-2023</p>
+          </div>
+          <div>
+            <p class="mb-0 pb-0">3D</p>
+            <p class="text-warning mb-0 pb-0">445</p>
+          </div>
+        </div>
+    </div>
+    <div class="bg-green p-3 rounded my-3">
+        <div class="d-flex justify-content-between">
+          <div class="text-center">
+            <p class="mb-0 pb-0">Date</p>
+            <p class="mb-0 pb-0">16-10-2023</p>
+          </div>
+          <div>
+            <p class="mb-0 pb-0">3D</p>
+            <p class="text-warning mb-0 pb-0">445</p>
+          </div>
+        </div>
+    </div>
+    <div class="bg-green p-3 rounded my-3">
+        <div class="d-flex justify-content-between">
+          <div class="text-center">
+            <p class="mb-0 pb-0">Date</p>
+            <p class="mb-0 pb-0">16-10-2023</p>
+          </div>
+          <div>
+            <p class="mb-0 pb-0">3D</p>
+            <p class="text-warning mb-0 pb-0">445</p>
+          </div>
+        </div>
+    </div>
+    <div class="bg-green p-3 rounded my-3">
+        <div class="d-flex justify-content-between">
+          <div class="text-center">
+            <p class="mb-0 pb-0">Date</p>
+            <p class="mb-0 pb-0">16-10-2023</p>
+          </div>
+          <div>
+            <p class="mb-0 pb-0">3D</p>
+            <p class="text-warning mb-0 pb-0">445</p>
+          </div>
+        </div>
+    </div>
+</div>
+
+<div class="row fixed-footer">
+    <div class="col mx-auto">
+      <a href="{{ url('/admin/three-d-play') }}" class="btn custom-btn btn-light">ထိုးမည်</a>
+    </div>
+</div>
+<!-- winner result -->
+@endsection
